c78c539818bbc6676abf1b3c2a09a0e097b986e9
Dupest <61519772+Dupest@users.noreply.github.com>
2020-12-07 16:39:20 -0500

    Hashmap

--- Code/src/Player.java
+++ Code/src/Player.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Player {
     
     private static final int DEFAULT_HEALTH = 15;
+    private HashMap<String, String> playersMap;
 
     //TODO: Pick one of these two
     private ArrayList<Key> keyList;
@@ -14,9 +16,10 @@ public class Player {
         healthPool = DEFAULT_HEALTH;
         keyList = new ArrayList();
         currentRoom = null;
+        playersMap = new HashMap<>();
     }
     
     public void trapTriggered(){
         healthPool--;
     }
-}
+    }
