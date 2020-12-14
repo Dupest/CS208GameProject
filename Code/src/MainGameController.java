4fa874aac5fc72d4210748d0d6ffc298dddfa880
Dupest <61519772+Dupest@users.noreply.github.com>
2020-12-14 18:39:17 -0500

    More Graphics done

--- Code/src/MainGameController.java
+++ Code/src/MainGameController.java
@@ -31,6 +31,9 @@ public class MainGameController {
     private HashMap<Integer, Key> keyMap;
     private HashMap<Integer, Player> playerMap;
 
+
+    private double individualGridPaneWidth;
+    private double individualGridPaneHeight;
     private final Color[] playerColors = {Color.VIOLET, Color.ORANGE, Color.RED, Color.BLACK};
 
     private static final int PADDING = 2;
@@ -136,7 +139,8 @@ public class MainGameController {
             winHeight = mainGridPane.getPrefHeight();
             winWidth = mainGridPane.getPrefWidth();
         }
-
+        individualGridPaneWidth = (winWidth/numColumns);
+        individualGridPaneHeight = (winHeight/numRows);
         //Another hacky solution that I've found. getChildren().clear() removes the gridlines on our gridpane. However, this information is stored within the very first
         //child so we simply store that through the deletion, and fit it back in to regain our lines.
         Node node = mainGridPane.getChildren().get(0);
@@ -156,32 +160,8 @@ public class MainGameController {
                 gc = newMapImage.getGraphicsContext2D();
                 Rectangle newRect = new Rectangle
                         (newMapImage.getWidth()/2.0, newMapImage.getHeight()/2.0, newMapImage.getWidth()-PADDING, newMapImage.getHeight()-PADDING);
-                
-//                System.out.println("W:" + newMapImage.getWidth());
-//                System.out.println("H:" + newMapImage.getHeight());
-
                 mapLayout[i][k].setRoomRender(newRect);
                 drawRectangle(gc, newRect);
-                
-                //My custom circle class from the last project we did because I wanted a quick lazy drawing to show this off.
-                //CircleWithText mapCircle = new CircleWithText("Map (" + i + ", " + k + ")", new Point2D(newMapImage.getWidth()/2.0,newMapImage.getHeight()/2.0));
-
-//                //Default radius on the circles is 40, but if we start to get smaller screen sizes to where each node only has 80 pixels of room in any direction, we adjust the radius.
-//                if(newMapImage.getHeight() < mapCircle.getDefRadius()*2 || newMapImage.getWidth() < mapCircle.getDefRadius()*2){
-//
-//                    //We take the smaller of the two values
-//                    double smallerBound = Math.min(newMapImage.getHeight(), newMapImage.getWidth());
-//                    //double smallerBound =(newMapImage.getHeight() > newMapImage.getWidth()) ? newMapImage.getWidth(): newMapImage.getHeight();
-//
-//                    //Radius becomes half of the smallest distance to an edge.
-//                    mapCircle.setRadius((int)(smallerBound/2.0));
-//                    mapCircle.drawCusRadius(gc);
-//                }
-//
-//                //Otherwise we draw it normally
-//                else
-//                    mapCircle.draw(gc);
-                //System.out.println("Circle coords: " + mapCircle.getPoint());
                 Group newGroup = new Group();
                 newGroup.getChildren().add(newMapImage);
                 mainGridPane.add(newGroup, i, k);
@@ -192,7 +172,7 @@ public class MainGameController {
         for(int i = 1; i < 81; i++){
             if(gameLogic.getRoom(i).isATrap()) {
                 Room currRoom = gameLogic.getRoom(i);
-                mainGridPane.add(drawTrap(new Rectangle()), currRoom.getX(), currRoom.getY());
+                //mainGridPane.add(drawTrap(new Rectangle(currRoom.getRoomRender().getX(), currRoom.getRoomRender().getY())), currRoom.getX(), currRoom.getY());
             }
         }
 
@@ -200,8 +180,8 @@ public class MainGameController {
         int[] xYOffsets = {0, 0};
 
         //Approximate width/height of each individual rectangle
-        double individualGridPaneWidth = (winWidth/numColumns);
-        double individualGridPaneHeight = (winHeight/numRows);
+        //double individualGridPaneWidth = (winWidth/numColumns);
+       // double individualGridPaneHeight = (winHeight/numRows);
 
         //Take the lesser of the two values ( should usually be the height, but..) and sets the radius to half that value (accounting for padding)
         double radius = Math.min(individualGridPaneHeight/gameLogic.getMaxPlayers(), individualGridPaneWidth/gameLogic.getMaxPlayers())-(PADDING/2.0);
@@ -220,29 +200,10 @@ public class MainGameController {
                xYOffsets[1]++;
             }
         }
-//        //Generates 4 Players.
-//        Circle player1Graphic = new Circle
-//                        (newMapImage.getWidth()/2.0, newMapImage.getHeight(),15);
-//        Circle player2Graphic = new Circle
-//                        (newMapImage.getWidth()/2.0, newMapImage.getHeight(),15);
-//        Circle player3Graphic = new Circle
-//                        (newMapImage.getWidth()/2.0, newMapImage.getHeight(),15);
-//        Circle player4Graphic = new Circle
-//                        (newMapImage.getWidth()/2.0, newMapImage.getHeight(),15);
-//        //sets colors of 4 players
-//        drawPlayer1(player1Graphic);
-//        drawPlayer2(player2Graphic);
-//        drawPlayer3(player3Graphic);
-//        drawPlayer4(player4Graphic);
-//        //adds 4 players to nodes the commented out code is how to add the players when we have them working.
-//        mainGridPane.add(player1Graphic, 0, 0); //mainGridPane.add(player1Graphic, GL.getPlayer(2).getX(), GL.getPlayer(1).getY());
-//        mainGridPane.add(player2Graphic, 0, 0); //mainGridPane.add(player2Graphic, GL.getPlayer(2).getX(), GL.getPlayer(1).getY());
-//        mainGridPane.add(player3Graphic, 0, 0); //mainGridPane.add(player3Graphic, GL.getPlayer(2).getX(), GL.getPlayer(1).getY());
-//        mainGridPane.add(player4Graphic, 0, 0); //mainGridPane.add(player4Graphic, GL.getPlayer(2).getX(), GL.getPlayer(1).getY());
         mainGridPane.add(startingPlayers, 0,0);
-        movePlayer(null);
+        //movePlayer(null);
         mainGridPane.add(drawKey(new Rectangle()), 7, 7);
-        movePlayer(null);
+        //movePlayer(null);
         
         //Groups just add an extra layer of organization. In this case not necessary, but trying to show of some of the syntax too
 
@@ -281,44 +242,17 @@ public class MainGameController {
                 rect.getY()-rect.getHeight()/2.0,
                 rect.getWidth(),
                 rect.getHeight());
-        gc.setFill(Color.GREEN);
+        gc.setLineWidth(15);
         gc.setStroke(Color.ORANGE);
     }
 
     //TODO make players able to be visable in rooms where they are as well as deleting them from rooms
-
-
-    //Sets the Colors and Sizes of different players
-        
-    private void drawPlayer1(Circle Circle){
-        Circle.setFill(javafx.scene.paint.Color.DARKBLUE);
-        Circle.setTranslateX(26);
-        Circle.setTranslateY(0);
-    }
-    
-    private void drawPlayer2(Circle Circle){
-        Circle.setFill(javafx.scene.paint.Color.CRIMSON);
-        Circle.setTranslateX(56);
-        Circle.setTranslateY(0);
-    }
-    
-    private void drawPlayer3(Circle Circle){
-        Circle.setFill(javafx.scene.paint.Color.BLUEVIOLET);
-        Circle.setTranslateX(86);
-        Circle.setTranslateY(0);
-    }
-    
-    private void drawPlayer4(Circle Circle){
-        Circle.setFill(javafx.scene.paint.Color.CORAL);
-        Circle.setTranslateX(116);
-        Circle.setTranslateY(0);
-    }
     //GUI to draw traps
     private Rectangle drawTrap(Rectangle Rect){
-        Rect.setWidth(50);
-        Rect.setHeight(50);
+        Rect.setWidth(individualGridPaneWidth/3.0);
+        Rect.setHeight(individualGridPaneHeight/3.0);
         Rect.setFill(Color.GREENYELLOW);
-        Rect.setTranslateX(58);
+        //Rect.setTranslateX(58);
         return Rect;
     }
     //GUI to draw keyse
